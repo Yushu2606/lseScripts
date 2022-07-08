91c62e6d64865b067a73cb2ccfadbf00f88ad5db
星梦喵吖 <starsdream00@icloud.com>
2022-07-08 16:49:04 +0800

    修复

--- Bazaar.js
+++ Bazaar.js
@@ -164,7 +164,7 @@ function shopManagement(pl) {
     fm.addButton("信息设置");
     fm.addButton("物品管理");
     fm.addButton("查看历史纪录");
-    fm.addButton(`${shop.name}§r\n-- 预览模式-- `, shop.icon);
+    fm.addButton(`${shop.name}§r\n-- 预览模式 -- `, shop.icon);
     pl.sendForm(fm, (pl, arg) => {
         switch (arg) {
             case 0:
@@ -175,9 +175,6 @@ function shopManagement(pl) {
                 break;
             case 2:
                 shopHistroy(pl);
-                break;
-            case 3:
-                itemList(pl, pl.xuid);
                 break;
             default:
                 main(pl);
@@ -289,10 +286,6 @@ function itemBuy(pl, owner, item) {
     pl.sendForm(fm, (pl, args) => {
         if (!args) {
             itemList(pl, owner);
-            return;
-        }
-        if (pl.xuid == owner) {
-            shopManagement(pl);
             return;
         }
         let shop = db.get(owner);
