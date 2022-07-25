d528b87514ccc5e2a06a692fae9153e4660dddf7
星梦StarsDream <starsdream00@icloud.com>
2022-07-25 18:24:40 +0800

    更新

--- BlockIslandAllocation.js
+++ BlockIslandAllocation.js
@@ -39,95 +39,87 @@ mc.listen("onDestroyBlock", (pl, bl) => {
     return re;
 });
 function sendInit(pl) {
-    pl.sendSimpleForm(
-        "欢迎来到方屿！",
-        "请选择类别",
-        ["经典单方块", "与他人合作"],
-        ["textures/ui/sword", "textures/ui/FriendsIcon"],
-        (pl, arg) => {
-            if (arg == null) return sendInit(pl);
-            switch (arg) {
-                case 0:
-                    pl.tell("您选择了「经典单方块」\n正在为您分配，请稍候…");
-                    const x = returnPos(true);
-                    const y = randomInt(96, 288);
-                    const z = returnPos(false);
-                    pl.setRespawnPosition(x, y + 1, z, 0);
-                    const timerid = setInterval(() => {
-                        if (
-                            mc.setBlock(x, y, z, 0, "minecraft:grass", 0) &&
-                            mc.setBlock(x, y + 1, z, 0, "minecraft:sapling", 0)
-                        )
-                            clearInterval(timerid);
-                        else pl.teleport(x, y + 1, z, 0);
-                    }, 50);
-                    db.set(pl.xuid, {
-                        version: "classic",
-                        pos: { x: x, y: y, z: z },
-                    });
-                    return pl.tell("分配完毕");
-                case 1:
-                    const options = [];
-                    for (const key of db.listKey()) {
-                        if (
-                            db.get(key).version == "team" ||
-                            db.get(key).version == "spawn" ||
-                            !mc.getPlayer(key)
-                        )
-                            continue;
-                        options.push(data.xuid2name(key));
-                    }
-                    if (options.length < 1) {
-                        pl.tell("§c暂无可组队用户");
+    const fm = mc.newSimpleForm();
+    fm.setTitle("开始菜单");
+    fm.setContent("欢迎来到方屿！");
+    fm.addButton("经典单方块", "textures/ui/sword");
+    fm.addButton("与在线用户组队", "textures/ui/FriendsIcon");
+    pl.sendForm(fm, (pl, arg) => {
+        if (arg == null) return sendInit(pl);
+        switch (arg) {
+            case 0:
+                pl.tell("您选择了「经典单方块」\n正在为您分配，请稍候……");
+                const x = returnPos(true);
+                const y = randomInt(96, 288);
+                const z = returnPos(false);
+                pl.setRespawnPosition(x, y + 1, z, 0);
+                const timerid = setInterval(() => {
+                    if (
+                        mc.setBlock(x, y, z, 0, "minecraft:grass", 0) &&
+                        mc.setBlock(x, y + 1, z, 0, "minecraft:sapling", 0)
+                    )
+                        clearInterval(timerid);
+                    else pl.teleport(x, y + 1, z, 0);
+                }, 50);
+                db.set(pl.xuid, {
+                    version: "classic",
+                    pos: { x: x, y: y, z: z },
+                });
+                return pl.tell("分配完毕");
+            case 1:
+                const options = [];
+                for (const key of db.listKey()) {
+                    if (
+                        db.get(key).version == "team" ||
+                        db.get(key).version == "spawn" ||
+                        !mc.getPlayer(key)
+                    )
+                        continue;
+                    options.push(data.xuid2name(key));
+                }
+                if (options.length < 1) {
+                    pl.tell("§c暂无可组队用户");
+                    return sendInit(pl);
+                }
+                const fm = mc.newCustomForm();
+                fm.setTitle("与在线用户组队");
+                fm.addDropdown("选择用户", options);
+                pl.sendForm(fm, (pl, args) => {
+                    if (!args) return sendInit(pl);
+                    const pl1 = mc.getPlayer(options[args[0]]);
+                    if (!pl1) {
+                        pl.tell(`§c${options[args[0]]}已离线`);
                         return sendInit(pl);
                     }
-                    const fm = mc.newCustomForm();
-                    fm.setTitle("与他人合作");
-                    fm.addDropdown("选择合作用户", options);
-                    pl.sendForm(fm, (pl, args) => {
-                        if (!args) return sendInit(pl);
-                        const pl1 = mc.getPlayer(options[args[0]]);
-                        if (!pl1) {
-                            pl.tell(`§c${options[args[0]]}已离线`);
-                            return sendInit(pl);
-                        }
-                        pl1.sendModalForm(
-                            "组队请求",
-                            `${pl.realName}请求与您组队`,
-                            "同意",
-                            "拒绝",
-                            (pl1, arg) => {
-                                if (!mc.getPlayer(pl.realName)) return;
-                                if (!arg) {
-                                    pl.tell(
-                                        `§c与${pl1.realName}的组队请求被拒绝`
-                                    );
-                                    return sendInit(pl);
-                                }
-                                const d2 = db.get(pl1.xuid);
-                                pl.setRespawnPosition(
-                                    d2.pos.x,
-                                    d2.pos.y + 1,
-                                    d2.pos.z,
-                                    0
-                                );
-                                pl.teleport(
-                                    d2.pos.x,
-                                    d2.pos.y + 1,
-                                    d2.pos.z,
-                                    0
-                                );
-                                db.set(pl.xuid, {
-                                    version: "team",
-                                    pos: d2.pos,
-                                });
-                                pl.tell(`与${pl1.realName}组队成功`);
+                    pl1.sendModalForm(
+                        "组队请求",
+                        `${pl.realName}请求与您组队`,
+                        "同意",
+                        "拒绝",
+                        (pl1, arg) => {
+                            if (!mc.getPlayer(pl.realName)) return;
+                            if (!arg) {
+                                pl.tell(`§c与${pl1.realName}的组队请求被拒绝`);
+                                return sendInit(pl);
                             }
-                        );
-                    });
-            }
+                            const d2 = db.get(pl1.xuid);
+                            pl.setRespawnPosition(
+                                d2.pos.x,
+                                d2.pos.y + 1,
+                                d2.pos.z,
+                                0
+                            );
+                            pl.teleport(d2.pos.x, d2.pos.y + 1, d2.pos.z, 0);
+                            db.set(pl.xuid, {
+                                version: "team",
+                                pos: d2.pos,
+                            });
+                            pl.tell(`与${pl1.realName}组队成功`);
+                        }
+                    );
+                });
         }
-    );
+    });
 }
 function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
