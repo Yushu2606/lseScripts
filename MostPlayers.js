ae7b648267e5274d063211cf92247f831f2366c0
星梦StarsDream <starsdream00@icloud.com>
2022-07-31 14:46:41 +0800

    修复

--- MostPlayers.js
+++ MostPlayers.js
@@ -4,6 +4,6 @@ ll.registerPlugin("MostPlayers", "最多同时在线人数API", [1, 0, 0]);
 const db = new KVDatabase("plugins\\MostPlayers\\data");
 mc.listen("onJoin", () => {
     const plcount = mc.getOnlinePlayers().length;
-    if (db.get("Count") ?? 0 < plcount) db.set("Count", plcount);
+    if ((db.get("Count") ?? 0) < plcount) db.set("Count", plcount);
 });
 ll.export(() => db.get("Count") ?? 0, "MostPlayers", "Get");
