9e1a03f6e6c7c18789eec4dba540da9e8be7f5e5
星梦 <starsdream00@icloud.com>
2023-08-29 12:04:43 +0800

    初始化

--- DestroyPushedRod.js
+++ DestroyPushedRod.js
@@ -1,6 +1,6 @@
 /*
 English:
-    Title
+    DestroyPushedRod
     Copyright (C) 2023  StarsDream00 starsdream00@icloud.com
 
     This program is free software: you can redistribute it and/or modify
@@ -17,7 +17,7 @@ English:
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 中文：
-    头衔
+    杆可被推动破坏
     版权所有 © 2023  星梦喵吖 starsdream00@icloud.com
     本程序是自由软件：你可以根据自由软件基金会发布的GNU Affero通用公共许可证的条款，即许可证的第3版，
     或（您选择的）任何后来的版本重新发布和/或修改它。
@@ -31,41 +31,8 @@ English:
 */
 
 "use strict";
-ll.registerPlugin("Title", "头衔", [1, 0, 0]);
+ll.registerPlugin("DestroyPushedRod", "杆可被推动破坏", [1, 0, 0]);
 
-const db = new KVDatabase("plugins/Title/data");
-const addCommand = mc.newCommand("addtitle", "添加头衔。");
-addCommand.mandatory("player", ParamType.Player);
-addCommand.mandatory("title", ParamType.String);
-addCommand.overload(["player", "title"]);
-addCommand.setCallback((_cmd, _ori, out, res) => {
-    for (const pl of res.player) {
-        pl.rename(`${res.title}\n${pl.realName}`);
-        db.set(pl.xuid, res.title);
-    }
-    return out.success(`已为${res.player.length}个玩家添加头衔${res.title}`);
+mc.listen("onPistonTryPush", (_pos, bl) => {
+    if (bl.type.endsWith("rod")) bl.destroy(true);
 });
-addCommand.setup();
-const removeCommand = mc.newCommand("removetitle", "移除头衔。");
-removeCommand.mandatory("player", ParamType.Player);
-removeCommand.overload(["player"]);
-removeCommand.setCallback((_cmd, _ori, out, res) => {
-    for (const pl of res.player) {
-        pl.rename(pl.realName);
-        db.delete(pl.xuid);
-    }
-    return out.success(`已为${res.player.length}个玩家移除头衔`);
-});
-removeCommand.setup();
-mc.listen("onChat", (pl, msg) => {
-    const title = db.get(pl.xuid);
-    if (!title) return;
-    mc.broadcast(`${title ? `[${title}}]` : ""}<${pl.realName}> ${msg}`);
-    return false;
-});
-mc.listen("onJoin", (pl) => {
-    const title = db.get(pl.xuid);
-    if (!title) return;
-    pl.rename(`${title}\n${pl.realName}`);
-});
-ll.exports((xuid) => db.get(xuid), "Title", "GetFromXuid");
